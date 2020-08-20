02b03df919b3ea36ed823988d078e6c0a0341ad4
jdavis3333 <jdavis3333@gmail.com>
2020-08-19 23:48:36 -0700

    Updates

---
+++ precache-manifest.f1ff5961ee8dd2f066352b7f31ba77a0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f00fb1131156c8f56b146181ab596c12",
+    "url": "/react_portfolio/index.html"
+  },
+  {
+    "revision": "77d3537ad434d429992b",
+    "url": "/react_portfolio/static/css/main.d5331800.chunk.css"
+  },
+  {
+    "revision": "df6d6185472915bf91bd",
+    "url": "/react_portfolio/static/js/2.f6e464ce.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react_portfolio/static/js/2.f6e464ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77d3537ad434d429992b",
+    "url": "/react_portfolio/static/js/main.ee4c38da.chunk.js"
+  },
+  {
+    "revision": "651c4343a2c615c3ebd6",
+    "url": "/react_portfolio/static/js/runtime-main.3235d0ce.js"
+  },
+  {
+    "revision": "28163ed9fb9728127d2a133df4355302",
+    "url": "/react_portfolio/static/media/Joe_Davis_resume_2020.c.28163ed9.pdf"
+  },
+  {
+    "revision": "556fdebc901d6038048f55a9beed31ac",
+    "url": "/react_portfolio/static/media/headshot.556fdebc.PNG"
+  },
+  {
+    "revision": "4c1cdaf3bcd9e753a4dce8edb0dde3dd",
+    "url": "/react_portfolio/static/media/lv.4c1cdaf3.jpg"
+  }
+]);
